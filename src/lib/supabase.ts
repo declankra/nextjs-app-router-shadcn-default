ae4eea15fb1aec39881f831ba7a8b7c369d931e5
declankra <declankramper@gmail.com>
2024-12-22 19:39:50 -0600

    only initialize it if it used

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+let supabase: ReturnType<typeof createClient>;
 
-if (!supabaseUrl || !supabaseAnonKey) {
+export const getSupabase = () => {
+  if (supabase) return supabase;
+
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Missing Supabase URL or anonymous key.');
   }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+  supabase = createClient(supabaseUrl, supabaseAnonKey);
+  return supabase;
+};
